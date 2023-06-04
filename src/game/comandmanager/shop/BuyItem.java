cfe3ce93625eaedb399c11fb7cff2f8eddefa430
Jan Poloch <Pigino17333@seznam.cz>
2023-06-04 17:56:21 +0200

    edited BuyItem

--- src/game/comandmanager/shop/BuyItem.java
+++ src/game/comandmanager/shop/BuyItem.java
@@ -3,17 +3,25 @@ package game.comandmanager.shop;
 import game.Item;
 import game.Player;
 import game.Room;
+import game.Shop;
 
 
 public class BuyItem implements ShopAction {
 
     @Override
-    public boolean execute(int itemIndex, Player player, Item... items) {
-        Item boughtItem = items[itemIndex];
+    public boolean execute(int itemIndex, Player player, Shop shop){
+        Item boughtItem = shop.getShopItems()[itemIndex];
         if(itemIndex == 0 && player.getCoins() < 15 || itemIndex == 1 && player.getCoins() < 10 || itemIndex == 2 && player.getCoins() < 5)
             return false;
         player.getPlayerInventory().add(boughtItem);
-        items[itemIndex] = null;
+        int currentCoins = player.getCoins();
+        switch (itemIndex)
+        {
+            case 0 -> player.setCoins(currentCoins - 15);
+            case 1 -> player.setCoins(currentCoins - 10);
+            case 2 -> player.setCoins(currentCoins - 5);
+        }
+        shop.getShopItems()[itemIndex] = null;
         return true;
     }
 }
--- src/game/comandmanager/shop/ShopAction.java
+++ src/game/comandmanager/shop/ShopAction.java
@@ -2,10 +2,11 @@ package game.comandmanager.shop;
 
 import game.Item;
 import game.Player;
+import game.Shop;
 
 import java.util.List;
 
 public interface ShopAction {
 
-    boolean execute(int itemIndex, Player player, Item... items);
+    boolean execute(int itemIndex, Player player, Shop shop);
 }
