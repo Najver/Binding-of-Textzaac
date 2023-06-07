9634de4388f413cd5ab969940c66adfa5121f046
Jan Poloch <Pigino17333@seznam.cz>
2023-06-07 23:26:10 +0200

    added colores

--- src/util/ConsoleColors.java
+++ src/util/ConsoleColors.java
@@ -2,7 +2,9 @@ package util;
 
 public enum ConsoleColors {
     RESET("\033[0m"),
-    GREEN("\033[0;32m");
+    GREEN("\033[0;32m"),
+    RED("\033[0;31m"),
+    BLUE("\033[0;34m");
     public final String colorCode;
 
     ConsoleColors(String colorCode) {
